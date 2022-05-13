9f0ad7f483d0f176bef9992c8662df4cea51cdfa
Demir Yerli <demiryerli@gmail.com>
2022-05-13 10:56:20 +0300

    init

---
+++ src/module.cs
@@ -0,0 +1,52 @@
+﻿namespace spm_module;
+
+public static class ModuleData{
+    public const string Name = "spm";
+    public const string Description = "SPM";
+    public const string Author = "";
+    public const string Version = "0.0.1";
+    public const float spm_api_compatability = 3.0f;
+}
+
+public class PreInstallHooks
+{
+    public static void PreInstallHook(string Package)
+    {
+
+    }
+}
+public class PostInstallHooks
+{
+    public static void PostInstallHook(string Package)
+    {
+
+    }
+}
+public class PreUninstallHooks
+{
+    public static void PreUninstallHook(string Package)
+    {
+
+    }
+}
+public class PostUninstallHooks
+{
+    public static void PostUninstallHook(string Package)
+    {
+
+    }
+}
+public class PreUpdateHooks
+{
+    public static void PreUpdateHook(string Package, int version)
+    {
+
+    }
+}
+public class PostUpdateHooks
+{
+    public static void PostUpdateHook(string Package, int version)
+    {
+
+    }
+}
